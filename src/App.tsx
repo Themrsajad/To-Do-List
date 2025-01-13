ce92a2612b618ce851ea5a8b10eb16ee4535d69e
Themrsajad <mardanisa10@gmail.com>
2025-01-14 01:40:37 +0330

    try to deploy1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from "./components/pages/home";
-import Completed from "./components/pages/completed";
+import Home from "./components/pages/Home";
+import Completed from "./components/pages/Completed";
 
 export default function App() {
   return (
